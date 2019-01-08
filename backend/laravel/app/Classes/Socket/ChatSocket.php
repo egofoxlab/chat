82a97a3da66fc4fe9cd9b22cb017b68f12b601f7
EGOFOXLAB <stasbamiza3@gmail.com>
2019-01-08 22:35:29 +0200

    First commit

---
+++ backend/laravel/app/Classes/Socket/ChatSocket.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Classess\Socket;
+
+use App\Classes\Socket\Base\BaseSocket;
+use Ratchet\ConnectionInterface;
+
+class ChatSocket extends BaseSocket {
+
+	protected $clientList;
+
+	public function __construct() {
+		$this->clientList = new \SplObjectStorage();
+	}
+
+	function onOpen(ConnectionInterface $conn) {
+		$this->clientList->attach($conn);
+
+		echo "New connection" . PHP_EOL;
+	}
+
+	function onClose(ConnectionInterface $conn) {
+		$this->clientList->detach($conn);
+
+		echo "Detach client. Current count: {$this->clientList->count()}" . PHP_EOL;
+
+		foreach ($this->clientList as $item) {
+			/** @var ConnectionInterface $client */
+			$client = $item;
+
+			$client->send("Someone detach.");
+		}
+	}
+
+	function onError(ConnectionInterface $conn, \Exception $e) {
+		echo "In client error occurred.";
+
+		$conn->close();
+	}
+
+	function onMessage(ConnectionInterface $from, $msg) {
+		foreach ($this->clientList as $item) {
+			/** @var ConnectionInterface $client */
+			$client = $item;
+
+			$client->send("Hey! Message from someone! `{$msg}`");
+		}
+	}
+
+}
---
+++ frontend/angular/src/app/app.component.ts
@@ -0,0 +1,69 @@
+import {AfterViewInit, Component, ElementRef} from '@angular/core';
+import * as $ from 'jquery';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements AfterViewInit {
+    title = 'frontend';
+
+    private elementRef;
+    private connection;
+
+    constructor(private _elementRef: ElementRef) {
+        this.elementRef = $(_elementRef.nativeElement);
+    }
+
+    ngAfterViewInit(): void {
+        this.initChat();
+    }
+
+    private initChat() {
+        this.connection = new WebSocket('ws://localhost:7000');
+
+        this.connection.onopen = (e: any) => {
+            console.log(`WS - OPEN CONNECTION`);
+        };
+
+        this.connection.onmessage = (e: any) => {
+            console.log(`WS - OPEN MESSAGE: ${e.data}`);
+
+            this.addMessage(e.data);
+        };
+    }
+
+    private addMessage(message: string) {
+        if (message === undefined || message === null) {
+            return;
+        }
+
+        message = message.trim();
+
+        if (message === '') {
+            return;
+        }
+
+        $('#message-container').append(`<div>${message}</div>`);
+    }
+
+    public eventSend(e) {
+        let input = this.elementRef.find('#chat-input').val();
+
+        if (input === undefined || input === null) {
+            alert('Fill input field!');
+        }
+
+        input = input.trim();
+
+        if (input === '') {
+            alert('Fill input field!');
+        }
+
+        this.connection.send(input);
+
+        this.elementRef.find('#chat-input').val('');
+    }
+
+}
